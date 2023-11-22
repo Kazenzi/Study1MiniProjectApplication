af6310598d079a1697dba940964565646b685c61
Kazenzi <jacintakazenzi99@gmail.com>
2023-11-22 16:35:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/study1miniprojectapplication/SignInMainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.study1miniprojectapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInMainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_main2)
+    }
+}
